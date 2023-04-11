03d468c1e8063affaf04d8e1d404349d72164a7a
FedeAle1525 <fedeale.1525@gmail.com>
2023-04-11 16:12:34 +0200

    PHP get data from DB

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
@@ -10,6 +11,18 @@ class PageController extends Controller
     // Metodo che ritorna la Vista "welcome"
     public function homepage()
     {
-        return view('welcome');
+        // Utilizza il Model 'Book' per recuperare tutti i libri della Tabella 'Books' tramite metodo statico 'all' che esegue una Query
+        // Ritorna una 'Collection' (Array di Dati molto potente) di Oggetti, ogni Oggetto corrisponde a una Riga della Tabella
+
+        $movies = Movie::all(); // 'all()' = SELECT * FROM movies
+
+        dd($movies);
+
+        // Passo alla Vista i risultato della Query
+        $data = [
+            'movies' => $movies
+        ];
+
+        return view('welcome', $data);
     }
 }
