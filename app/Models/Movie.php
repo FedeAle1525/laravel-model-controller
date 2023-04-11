733dcf7f22024925e5da4529b6d56d8747619cf3
FedeAle1525 <fedeale.1525@gmail.com>
2023-04-11 15:49:24 +0200

    FEAT create Model 'Movie' to rappresent ... ... entities of Table 'Movies'

---
+++ app/Models/Movie.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+// Model che rappresenta una entità della Tabella 'Movies'
+class Movie extends Model
+{
+    use HasFactory;
+}
