6ded417a31c9c21e4550cb16ec3e20f605c4879a
FedeAle1525 <fedeale.1525@gmail.com>
2023-04-11 15:40:58 +0200

    FEAT create Controller for Route of View 'Welcome'

---
+++ app/Http/Controllers/Guest/PageController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Guest;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    // Metodo che ritorna la Vista "welcome"
+    public function homepage()
+    {
+        return view('welcome');
+    }
+}
